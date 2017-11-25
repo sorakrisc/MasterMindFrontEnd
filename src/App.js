31fd48a5e3caad96751ffe0b205156be256b3fbc
James Sorakris <csorakris@gmail.com>
2017-11-25 17:14:06 +0700

    random lobby id

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends React.Component {
 
     constructor(props){
         super(props);
-        this.state={name: "no value", id: "", lobbyid: "", regform: false, logform: true, logClass: "login-form", regClass: "register-form", active: 0};
+        this.state={name: "no value", id: "", lobbyid: "", regform: false, logform: true, logClass: "login-form", regClass: "register-form", active: 0, generatedLobID:""};
         this.updateInputValue = this.updateInputValue.bind(this);
         this.updateInputLobby = this.updateInputLobby.bind(this);
+        this.makeLobbyID = this.makeLobbyID.bind(this);
 
-        this.handleClick = this.handleClick.bind(this);
     }
 
     updateInputValue(evt){
@@ -55,57 +55,58 @@ class App extends React.Component {
             console.log(error);
         });
     }
+    makeLobbyID(){
+        var vowel ="aeiou";
+        var consonant = "bcdfghjklmnpqrstvwxyz";
+        return consonant[Math.floor(Math.random()*21)]+vowel[Math.floor(Math.random()*5)]+consonant[Math.floor(Math.random()*21)]+consonant[Math.floor(Math.random()*21)]+vowel[Math.floor(Math.random()*5)]+consonant[Math.floor(Math.random()*21)];
+    }
+    componentDidUpdate(){
+
+        if(this.state.logform && window.location.href =="http://localhost:3000/login#register-form" ){
+            this.setState({regClass: "register-form show", logform: false, regform: true, active: this.state.active+1, generatedLobID: this.makeLobbyID()})
+        }
+        else if (this.state.regform && window.location.href =="http://localhost:3000/login#login-form"){
+            this.setState({regClass: "register-form", logform: true, regform: false,active: this.state.active+1})
+        }
 
 
-  handleClick() {
-    this.setState({active: !this.state.active});
-  }
-  componentDidUpdate(){
+    }
 
-            if(this.state.logform && window.location.href =="http://localhost:3000/login#register-form" ){
-                this.setState({regClass: "register-form show", logform: false, regform: true, active: this.state.active+1})
-            }
-            else if (this.state.regform && window.location.href =="http://localhost:3000/login#login-form"){
-               this.setState({regClass: "register-form", logform: true, regform: false,active: this.state.active+1})
-            }
-
-
-  }
-  component
     render() {
         //so when people access http://localhost:3000/login#register-form directly it make the register page visible
         if(this.state.active == 0 &&this.state.logform && window.location.href =="http://localhost:3000/login#register-form" ){
-            this.setState({regClass: "register-form show", logform: false, regform: true, active: this.state.active+1})
+            this.setState({regClass: "register-form show", logform: false, regform: true, active: this.state.active+1, generatedLobID: this.makeLobbyID()})
         }
         return (
-              <div className="App">
+            <div className="App">
 
-                    <header className="App-header">
-                        <h1 className="App-title">Welcome to Master mind</h1>
-                    </header>
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to Master mind</h1>
+                </header>
 
-                    <div className="login-page">
-                        <div className="form" >
-                            <form  id="login-form" className={this.state.regClass} >
-                                <input type="text" placeholder="name"/>
-                                <div>
-                                    <p>Lobby ID:</p>
-                                    <textarea readOnly id="code-content" className="lobby-code" ></textarea>
-                                </div>
-                                <button>create</button>
-                                <p className="message">Got a lobby? <a href="#login-form">Join a lobby</a></p>
-                            </form>
+                <div className="login-page">
+                    <div className="form" >
 
-                            <form id="register-form" className={this.state.logClass} >
-                                <input type="text" placeholder="name" onChange={this.updateInputValue}/>
-                                <input type="text" placeholder="lobby id" onChange={this.updateInputLobby}/>
-                                <button onClick ={ (e) => this.onNavigateGame(e) }> Play! </button>
-                                <p className="message">No lobby? <a href="#register-form" >Create lobby</a></p>
-                            </form>
+                        <form  id="login-form" className={this.state.regClass} >
+                            <input type="text" placeholder="name"/>
+                            <div>
+                                <p>Lobby ID: {this.state.generatedLobID}</p>
 
-                        </div>
+                            </div>
+                            <button>create</button>
+                            <p className="message">Got a lobby? <a href="#login-form">Join a lobby</a></p>
+                        </form>
+
+                        <form id="register-form" className={this.state.logClass} >
+                            <input type="text" placeholder="name" onChange={this.updateInputValue}/>
+                            <input type="text" placeholder="lobby id" onChange={this.updateInputLobby}/>
+                            <button onClick ={ (e) => this.onNavigateGame(e) }> Play! </button>
+                            <p className="message">No lobby? <a href="#register-form" >Create lobby</a></p>
+                        </form>
+
                     </div>
-              </div>
+                </div>
+            </div>
         );
     }
 }
